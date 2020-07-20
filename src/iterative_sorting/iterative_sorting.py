72b0e3f5e753a9429eaae3208b22f0a229365b8d
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-07-20 16:46:52 -0700

    got sprint for stretch code interview mostly working

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,20 +1,37 @@
+nums = [26, 52, 16, 4, 28,3, 6]
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
+    new_arr = []
+    for i in range(len(arr) - 1): 
+        for i in arr:
+            cur_index = i
+            lowest = cur_index
+            if cur_index < lowest:
+                lowest = cur_index
+            if i == arr[len(arr) - 1]:
+                    print(lowest)
+                    new_arr.append(lowest)
+                    arr.pop(lowest)
+    return new_arr                
+
+print(selection_sort(nums))
+
+# print(selection_sort(nums))
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
+    # for i in range(0, len(arr) - 1):
+    #     cur_index = i
+    #     smallest_index = cur_index
+    #     # TO-DO: find next smallest element
+    #     # (hint, can do in 3 loc)
+    #     # Your code here
+    #     if cur_index < smallest_index:
+    #         start += 1
+    #         smallest_index = cur_index
 
+    #     # TO-DO: swap
+    #     # Your code here
 
-        # TO-DO: swap
-        # Your code here
-
-    return arr
-
-
+    # return arr
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -7,8 +7,20 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = arr[0]
+    high = arr[len(arr) - 1]
+    mid = (high - low) / 2
+    while low <= high:
+        mid = (high - low) / 2
+        if target == arr[mid]:
+            return mid
+        
+        if target < arr[mid]:
+            high = mid - 1
+        elif target > arr[mid]:
+            low = mid + 1
 
-    # Your code here
 
+sorted_nums = [3, 4, 6, 26, 28, 52, 55]
 
-    return -1  # not found
+binary_search(sorted_nums, 52)
