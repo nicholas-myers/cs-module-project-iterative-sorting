822c2e38b66647c0eba11f2b5a643e118ce8be1c
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-07-21 13:10:21 -0700

    went through and rewrote code to double check understanding

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,43 +1,74 @@
-nums = [26, 52, 16, 4, 28,3, 6]
-# TO-DO: Complete the selection_sort() function below
+arr = [5, 55, 6, 67, 12, 9, 25, 43, 16]
+# runtime: O(n * n) = O(n^2)
 def selection_sort(arr):
-    new_arr = []
-    for i in range(len(arr) - 1): 
-        for i in arr:
-            cur_index = i
-            lowest = cur_index
-            if cur_index < lowest:
-                lowest = cur_index
-            if i == arr[len(arr) - 1]:
-                    print(lowest)
-                    new_arr.append(lowest)
-                    arr.pop(lowest)
-    return new_arr                
+    # iterate through the array (represents the sorted-unsorted start 
+    # moving across the array)
+    for i in range(len(arr)):  # O(n)
+        # index of the start, as well as the index we're going to 
+        # swap the smallest element with 
+        start = i
 
-print(selection_sort(nums))
+        smallest_value = arr[start]
+        smallest_index = start
+        # finding the smallest element 
+        # in the "unsorted" portion of the array 
+        for unsorted_index in range(start, len(arr)):  # O(n)
+            if arr[unsorted_index] < smallest_value:
+                smallest_value = arr[unsorted_index]
+                smallest_index = unsorted_index
 
-# print(selection_sort(nums))
-    # loop through n-1 elements
-    # for i in range(0, len(arr) - 1):
-    #     cur_index = i
-    #     smallest_index = cur_index
-    #     # TO-DO: find next smallest element
-    #     # (hint, can do in 3 loc)
-    #     # Your code here
-    #     if cur_index < smallest_index:
-    #         start += 1
-    #         smallest_index = cur_index
+        # `smallest_index` is the index of the smallest element in the unsorted portion 
 
-    #     # TO-DO: swap
-    #     # Your code here
+        # once that's found, swap it with the element on the right edge 
+        # of the sorted-unsorted start 
 
-    # return arr
-# TO-DO:  implement the Bubble Sort function below
-def bubble_sort(arr):
-    # Your code here
-
+        arr[start], arr[smallest_index] = arr[smallest_index], arr[start]
 
     return arr
+
+
+arr = [5, 55, 6, 67, 12, 9, 25, 43, 16]
+print(selection_sort(arr))
+
+# def selection_sort(arr):
+    # new_arr = []
+    # # loop for as long as the arr has items
+    # for i in range(len(arr)):
+    #     # print(arr)
+    #     lowest_value = arr[0]
+    #     lowest_index = 0
+    #     for index, num in enumerate(arr, 0):
+    #         if num < lowest_value:
+    #             lowest_value = arr[index]
+    #             lowest_index = index
+    #     new_arr.append(lowest_value)
+    #     arr.pop(lowest_index)
+    # return new_arr
+
+# import random
+# arr4 = random.sample(range(200), 50)
+# print(selection_sort(arr4))
+
+# TO-DO:  implement the Bubble Sort function below
+def bubble_sort(arr):
+    values_swapped = True
+    # checks if values were swapped if they were swapped keep running
+    while values_swapped:
+        # set the default to be not swapped
+        values_swapped = False
+        # check every index minus the last index
+        for i in range(len(arr) - 1):
+            # current value is greater than the next value swap them
+            if arr[i] > arr[i+1]:
+                # swap them
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                # if they get swapped the while loop keeps happening until there are no more swaps
+                values_swapped = True
+    
+    return arr
+
+arr = [5,1,7,6,2,1,0]
+# print(bubble_sort(arr))
 
 '''
 STRETCH: implement the Counting Sort function below
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,26 +1,37 @@
 def linear_search(arr, target):
     # Your code here
-
+    # check every index value
+    for i in range(len(arr)): 
+  
+        if arr[i] == target: 
+            return i 
 
     return -1   # not found
 
 
-# Write an iterative implementation of Binary Search
+# Returns either the index of the target in the arr 
+# or, if the target isn't in the arr, return -1 
 def binary_search(arr, target):
-    low = arr[0]
-    high = arr[len(arr) - 1]
-    mid = (high - low) / 2
-    while low <= high:
-        mid = (high - low) / 2
-        if target == arr[mid]:
-            return mid
-        
-        if target < arr[mid]:
-            high = mid - 1
-        elif target > arr[mid]:
-            low = mid + 1
-
-
-sorted_nums = [3, 4, 6, 26, 28, 52, 55]
-
-binary_search(sorted_nums, 52)
+    # set the start and end points
+    start = 0
+    end = len(arr) - 1
+    
+    # if the start point is less than or equal to the endpoint keep checking
+    while   start <= end:
+        # get the middle index
+        midindex = (end + start) // 2
+        # if the middle index is the target return
+        if target == arr[midindex]:
+            return midindex
+        # if the target is less than the mid go left
+        if target < arr[midindex]:
+            # reset the end index ti mid minus 1
+            end = midindex - 1
+        # if the target is greater than mid go right
+        if target > arr[midindex]:
+            # reset the start index to be the mid plus one
+            start = midindex + 1
+    return -1
+    
+arr = [3, 4, 6, 16, 26, 28, 52, 55]
+# print(binary_search(arr, 58))
